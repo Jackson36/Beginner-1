531b2d98c8cade1549a1964bdd0229fbfd975e9d
Jackson36 <36062758+Jackson36@users.noreply.github.com>
2018-02-06 10:45:43 +0530

    Create prime_interval.py

---
+++ prime_interval.py
@@ -0,0 +1,12 @@
+def main():
+    a=input()
+    b=input()
+    for n in range(a,b+1):
+        if n > 1:
+            for i in range(2,n):
+                if (n%i)==0:
+                    break
+            else:
+                print(n)
+if __name__ == '__main__':
+    main()
