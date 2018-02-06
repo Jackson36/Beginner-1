1c1f042195843f3bfc8fc981bd701b35df03dfa0
Jackson36 <36062758+Jackson36@users.noreply.github.com>
2018-02-06 11:11:42 +0530

    Update armstrong_interval.py

--- armstrong_interval.py
+++ armstrong_interval.py
@@ -3,16 +3,16 @@ def main():
     a=input()
     b=input()
 
-    for num in range(a,b+1):
+    for n in range(a,b+1):
         sum = 0
-        temp = num
+        temp = n
         while temp > 0:
             digit=temp%10
             sum=sum+(digit**3)
             temp//=10
 
-        if num == sum:
-            print(num)
+        if n == sum:
+            print(n)
 
 if __name__ == '__main__':
     main()
